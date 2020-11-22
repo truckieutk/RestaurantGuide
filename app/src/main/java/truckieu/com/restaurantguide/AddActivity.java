609c4c2e72d85d55d42c4e79589bc2e7e7bfd86b
truckieutk <jasminetruckieu@gmail.com>
2020-11-22 14:50:16 -0500

    Initial commit

---
+++ app/src/main/java/truckieu/com/restaurantguide/AddActivity.java
@@ -0,0 +1,78 @@
+package truckieu.com.restaurantguide;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+
+public class AddActivity extends AppCompatActivity {
+    private TextView title;
+    private EditText name, address, phone, description;
+
+    private RatingBar ratingBar;
+    private Button submit;
+
+    String[] tag = { "Vegetarian","Fast Food","Asian Cuisine","Western Cuisine","BBQ","Dessert","Drinks" };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        submit = findViewById(R.id.submit);
+        title = findViewById(R.id.title);
+        name = findViewById(R.id.name);
+        address = findViewById(R.id.address);
+        phone = findViewById(R.id.phone);
+        //tag = findViewById(R.id.tag);
+        description = findViewById(R.id.description);
+        ratingBar = findViewById(R.id.rating);
+
+        //Create Array Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, tag);
+        //Find TextView control
+        AutoCompleteTextView acTextView = (AutoCompleteTextView) findViewById(R.id.tag);
+        //Set the number of characters the user must type before the drop down list is shown
+        acTextView.setThreshold(1);
+        //Set the adapter
+        acTextView.setAdapter(adapter);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void Home(){
+        Intent start = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(start);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_menu,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Home();
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/truckieu/com/restaurantguide/MainActivity.java
@@ -0,0 +1,65 @@
+package truckieu.com.restaurantguide;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+        switch (id){
+            case R.id.home:
+                break;
+            case R.id.add:
+                AddActivity();
+                break;
+            case R.id.search:
+                break;
+            case R.id.about:
+                break;
+        }
+        return super .onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.contexts_menu,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.edit:
+                break;
+            case R.id.delete:
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+    private void AddActivity(){
+        Intent start = new Intent(getApplicationContext(),AddActivity.class);
+        startActivity(start);
+    }
+
+}
