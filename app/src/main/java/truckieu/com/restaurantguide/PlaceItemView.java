b8e8beb2c677bef79494bfa9f8c24a824a65be9d
truckieutk <jasminetruckieu@gmail.com>
2020-11-27 14:03:41 -0500

    fix Array List and add more Buttons

--- app/src/main/java/truckieu/com/restaurantguide/PlaceItemView.java
+++ app/src/main/java/truckieu/com/restaurantguide/PlaceItemView.java
@@ -1,12 +1,14 @@
 package truckieu.com.restaurantguide;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PlaceItemView extends RecyclerView.ViewHolder {
     public TextView name, address, phone, description, tag, rating;
+    public Button edit, map, delete;
 
     public PlaceItemView(View view){
         super(view);
@@ -16,6 +18,9 @@ public class PlaceItemView extends RecyclerView.ViewHolder {
         description = view.findViewById(R.id.description);
         tag=view.findViewById(R.id.tag);
         rating=view.findViewById(R.id.rating);
+        edit=view.findViewById(R.id.edit);
+        map=view.findViewById(R.id.map);
+        delete=view.findViewById(R.id.delete);
     }
 
 }
