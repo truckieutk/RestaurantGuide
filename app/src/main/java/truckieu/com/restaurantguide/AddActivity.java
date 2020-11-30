bad9d5f4c38eb7e4f33f55ff2569498033bcf14d
truckieutk <jasminetruckieu@gmail.com>
2020-11-30 00:29:03 -0500

    fix Edit Delete and screens

--- app/src/main/java/truckieu/com/restaurantguide/AddActivity.java
+++ app/src/main/java/truckieu/com/restaurantguide/AddActivity.java
@@ -6,7 +6,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.content.Intent;
@@ -15,11 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 
 
 public class AddActivity extends AppCompatActivity {
@@ -27,9 +25,6 @@ public class AddActivity extends AppCompatActivity {
     private EditText name, address, phone, description;
     private RatingBar ratingBar;
     private Button submit;
-
-    ListView lvRes;
-    ArrayList<String> arrayInfo;
 
     String[] tag = { "Vegetarian","Fast Food","Asian Cuisine","Western Cuisine","BBQ","Dessert","Drinks" };
 
@@ -42,45 +37,21 @@ public class AddActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         address = findViewById(R.id.address);
         phone = findViewById(R.id.phone);
+        //tag = findViewById(R.id.tag);
         description = findViewById(R.id.description);
-        ratingBar = (RatingBar) findViewById(R.id.rating);
-
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, tag);
+        ratingBar = findViewById(R.id.rating);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, tag);
         //Find TextView control
-        final AutoCompleteTextView acTextView = (AutoCompleteTextView) findViewById(R.id.tag);
+        AutoCompleteTextView acTextView = (AutoCompleteTextView) findViewById(R.id.tag);
         //Set the number of characters the user must type before the drop down list is shown
         acTextView.setThreshold(1);
         //Set the adapter
         acTextView.setAdapter(adapter);
 
-        final ArrayAdapter<String> restaurant = new ArrayAdapter<>(
-                AddActivity.this, android.R.layout.simple_list_item_1, arrayInfo
-        );
-        lvRes.setAdapter(adapter);
-
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               String sName = name.getText().toString();
-               String sAddress = "Address:" + address.getText().toString();
-               String sPhone = "Phone:" + phone.getText().toString();
-               String sDescription= "Description:" + description.getText().toString();
-               //get the value from rating bar and display it into a toast
-               String totalStars = "Rating:" + ratingBar.getNumStars();
-               String rating = "Rating : " + ratingBar.getRating();
-               Toast.makeText(getApplicationContext(), totalStars + "\n" + rating, Toast.LENGTH_LONG).show();
-               String tag = "Tag: " + acTextView.getAdapter();
 
-
-               arrayInfo.add(sName);
-               arrayInfo.add(sAddress);
-               arrayInfo.add(sPhone);
-               arrayInfo.add(sDescription);
-               arrayInfo.add(tag);
-               arrayInfo.add(rating);
-
-               restaurant.notifyDataSetChanged();
             }
         });
     }
